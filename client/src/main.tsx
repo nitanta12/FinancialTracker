20cc0e8afdb22c9570a6f3669a0432028c619261
nitanta12 <nitanta789@gmail.com>
2025-10-25 13:31:17 +0545

    feat: set up the css and shadcn ui for the project

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
+import './index.css'; 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
+import App from './App';
+// Must point to Tailwind entry file
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
